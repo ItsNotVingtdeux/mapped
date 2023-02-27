9c014aa532a085e72e74720f1c00f0c22e947dd2
Pierre-Loup <pierreloup22@gmail.com>
2023-02-27 16:03:20 +0100

    Added very, very imprecise rotation

--- src/main/java/net/itsvingtdeux/mapped/TestHud.java
+++ src/main/java/net/itsvingtdeux/mapped/TestHud.java
@@ -1,6 +1,7 @@
 package net.itsvingtdeux.mapped;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.client.gui.overlay.IGuiOverlay;
@@ -16,18 +17,28 @@ public class TestHud {
     public static final IGuiOverlay HUD_TEST = ((gui, poseStack, partialTick, width, height) -> {
         int pixelSize = 5;
         int pixelsInMap = 11;
+        LocalPlayer myPlayer = Minecraft.getInstance().player;
+        float myAngle = (myPlayer.getYRot() - 180) % 360;
+        if(myAngle < 0){
+            myAngle += 360;
+        }
+        float myRad = myAngle * (float)Math.PI / 180.0F;
+        mapped.LOGGER.info(String.format("player rot: %f", myAngle));
         for(int x = 0; x < pixelsInMap; x++) {
             for(int y = 0; y < pixelsInMap; y++) {
-                double flX = Minecraft.getInstance().player.getX() + x - pixelsInMap / 2;
-                double flY = Minecraft.getInstance().player.getY() - 0.5;
-                double flZ = Minecraft.getInstance().player.getZ() + y - pixelsInMap / 2;
+                float searchX = x - pixelsInMap / 2;
+                float searchY = y - pixelsInMap / 2;
+                double searchXR = searchX * Math.cos(myRad) - searchY * Math.sin(myRad);
+                double searchYR = searchY * Math.cos(myRad) + searchX * Math.sin(myRad);
+                double flX = myPlayer.getX() + searchXR;
+                double flY = myPlayer.getY() - 0.5;
+                double flZ = myPlayer.getZ() + searchYR;
                 BlockPos myPosition = new BlockPos(flX, flY, flZ);
                 BlockState myState = Minecraft.getInstance().level.getBlockState(myPosition);
                 int myColor = myState.getMaterial().getColor().calculateRGBColor(MaterialColor.Brightness.NORMAL);
                 float myR = (myColor & 0xFF) / 255.0F;
                 float myG = ((myColor & 0xFF00) >>> 8) / 255.0F;
                 float myB = ((myColor & 0xFF0000) >>> 16) / 255.0F;
-                mapped.LOGGER.info(String.format("block color %d", myColor));
                 RenderSystem.setShader(GameRenderer::getPositionTexShader);
                 RenderSystem.setShaderColor(myR, myG, myB, 1.0F);
                 RenderSystem.setShaderTexture(0, RESOURCE_WHITE);
