602d21dda4329f535c267bc8b54f4849f6b8908f
Pierre-Loup <pierreloup22@gmail.com>
2023-02-27 14:55:04 +0100

    Turned the color indicator into a minimap somehow

--- src/main/java/net/itsvingtdeux/mapped/TestHud.java
+++ src/main/java/net/itsvingtdeux/mapped/TestHud.java
@@ -14,21 +14,25 @@ public class TestHud {
             "textures/gui/minimap/white.png");
 
     public static final IGuiOverlay HUD_TEST = ((gui, poseStack, partialTick, width, height) -> {
-        int x = width / 2;
-        int y = height;
-        double flX = Minecraft.getInstance().player.getX();
-        double flY = Minecraft.getInstance().player.getY() - 0.5;
-        double flZ = Minecraft.getInstance().player.getZ();
-        BlockPos myPosition = new BlockPos(flX, flY, flZ);
-        BlockState myState = Minecraft.getInstance().level.getBlockState(myPosition);
-        int myColor = myState.getMaterial().getColor().calculateRGBColor(MaterialColor.Brightness.NORMAL);
-        float myR = (myColor & 0xFF) / 255.0F;
-        float myG = ((myColor & 0xFF00) >>> 8) / 255.0F;
-        float myB = ((myColor & 0xFF0000) >>> 16) / 255.0F;
-        mapped.LOGGER.info( String.format("block color %d", myColor) );
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        RenderSystem.setShaderColor(myR, myG, myB, 1.0F);
-        RenderSystem.setShaderTexture(0, RESOURCE_WHITE);
-        GuiComponent.blit( poseStack, 0, 0, 0, 0,20,20,1,1);
+        int pixelSize = 5;
+        int pixelsInMap = 11;
+        for(int x = 0; x < pixelsInMap; x++) {
+            for(int y = 0; y < pixelsInMap; y++) {
+                double flX = Minecraft.getInstance().player.getX() + x - pixelsInMap / 2;
+                double flY = Minecraft.getInstance().player.getY() - 0.5;
+                double flZ = Minecraft.getInstance().player.getZ() + y - pixelsInMap / 2;
+                BlockPos myPosition = new BlockPos(flX, flY, flZ);
+                BlockState myState = Minecraft.getInstance().level.getBlockState(myPosition);
+                int myColor = myState.getMaterial().getColor().calculateRGBColor(MaterialColor.Brightness.NORMAL);
+                float myR = (myColor & 0xFF) / 255.0F;
+                float myG = ((myColor & 0xFF00) >>> 8) / 255.0F;
+                float myB = ((myColor & 0xFF0000) >>> 16) / 255.0F;
+                mapped.LOGGER.info(String.format("block color %d", myColor));
+                RenderSystem.setShader(GameRenderer::getPositionTexShader);
+                RenderSystem.setShaderColor(myR, myG, myB, 1.0F);
+                RenderSystem.setShaderTexture(0, RESOURCE_WHITE);
+                GuiComponent.blit(poseStack, x * pixelSize, y * pixelSize, 0, 0, pixelSize, pixelSize, 1, 1);
+            }
+        }
     });
 }
